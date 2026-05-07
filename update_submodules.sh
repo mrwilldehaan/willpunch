c7cd9018b5fbfe7313f1849e5731683fb6f18445
Will DeHaan <willdehaan@gmail.com>
2026-05-07 09:46:39 -0700

    submodules clone from holepunchto

---
+++ update_submodules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# import submodules from github
+#
+git_parent="https://github.com/holepunchto"
+
+while read subrepo; do
+	if [ ! -d "$subrepo" ]; then
+		echo git submodule add "$git_parent/$subrepo"
+		git submodule add "$git_parent/$subrepo"
+	fi
+done <repositories.holepunchto.github.com
+
